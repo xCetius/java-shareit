c1b1317573aa0a88532cc88ecc8b70877d33e6bf
Cetius <heajler@gmail.com>
2025-07-24 19:49:49 +0300

    Удалить тест

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,15 +1,10 @@
 package ru.practicum.shareit;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ShareItTests {
 
-	@Test
-	@Disabled
-	void contextLoads() {
-	}
 
 }
